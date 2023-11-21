a6fc42cac4b80a24ef8e320a19490bd62269bc18
hamishlawson <heymish96@gmail.com>
2023-11-21 20:04:47 +0000

    initial thoughts on project

---
+++ main.js
@@ -0,0 +1,10 @@
+// Random Message Generator 
+/* 
+Needs to take an input from the terminal and output something from three different data structures.
+example 
+"Hi how are you doing, Input your brith day"
+-- Receive Input
+"Nice according to the mad world of modern theology that means you are [data structure one] type of 
+[data structure two] and have the following characteristics [data structure 3]"
+*/
+
